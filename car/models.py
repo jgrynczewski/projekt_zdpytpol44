5d8c8b9769c0de698c599abbc241f95a0e68a162
jgrynczewski <Tel^)%322))$>
2022-01-11 19:31:49 +0100

    migrations 2

--- car/models.py
+++ car/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Car(models.Model):
     name = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=9, decimal_places=2)
+    added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
