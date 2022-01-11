15486c6c59394f73e5e5a4c79c6321f87c68fbf1
jgrynczewski <Tel^)%322))$>
2022-01-11 19:05:47 +0100

    Create models

--- car/models.py
+++ car/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
